d4588f49caac3930d145203b6597119a626c6485
geekNero <sg550js@gmail.com>
2023-08-24 17:46:26 +0530

    corrected code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-a = subprocess.run(['ls'], capture_output=True)
+a = subprocess.run(['ls'], capture_output=True, text=True)
 output_path = os.getenv('GITHUB_OUTPUT')
 with open(output_path, "a") as myfile:
-    myfile.write(a.stdout)
+    myfile.write(f"{a.stdout}")
