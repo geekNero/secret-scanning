e71cdbef8fffd12ba5391a5cb4eff448807b1665
geekNero <sg550js@gmail.com>
2023-08-24 17:38:04 +0530

    changed output format

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
 
 a = subprocess.run(['ls'], capture_output=True)
-print(a.stdout)
+output_path = os.getenv('GITHUB_OUTPUT')
+with open(output_path, "a") as myfile:
+    myfile.write(f"{a.stdout} Hey the program ran successfully")
